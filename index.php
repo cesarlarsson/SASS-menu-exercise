217ab717d1e71aabb15059c9ea03adbb6d8bf8fc
cesar suarez <cesarlarsson@gmail.com>
2015-03-17 21:13:23 -0500

    ejercicio inicial

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="estilos/estilo.css">
+    </head>
+    <body>
+<div id="header">
+			<ul class="nav">
+				<li><a href="">Inicio</a></li>
+				<li><a href="">Servicios</a>
+					<ul>
+						<li><a href="">Submenu1</a></li>
+						<li><a href="">Submenu2</a></li>
+						<li><a href="">Submenu3</a></li>
+						<li><a href="">Submenu4</a>
+							<ul>
+								<li><a href="">Submenu1</a></li>
+								<li><a href="">Submenu2</a></li>
+								<li><a href="">Submenu3</a></li>
+								<li><a href="">Submenu4</a></li>
+							</ul>
+						</li>
+					</ul>
+				</li>
+				<li><a href="">Acerca de</a>
+					<ul>
+						<li><a href="">Submenu1</a></li>
+						<li><a href="">Submenu2</a></li>
+						<li><a href="">Submenu3</a></li>
+						<li><a href="">Submenu4</a></li>
+					</ul>
+				</li>
+				<li><a href="">Contacto</a></li>
+			</ul>
+		</div>
+        
+        <div class="wrapper">
+            <div class="for-1">Elemento 1</div>
+            <div class="for-2">Elemento 2</div>
+            <div class="for-3">Elemento 3</div>
+            <div class="for-4">Elemento 4</div>
+            <div class="for-5">Elemento 5</div>
+            <div class="for-6">Elemento 6</div>
+            <div class="for-7">Elemento 7</div>
+            <div class="for-8">Elemento 8</div>
+            <div class="for-9">Elemento 8</div>
+            <div class="for-10">Elemento 8</div>
+            
+            
+        </div>
+    </body>
+</html>
